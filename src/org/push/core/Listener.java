ba1e8f3fd175f10816872384bd818377cb09996d
Lei Wang <wanly3643@gmail.com>
2014-10-29 14:11:48 +0800

    Combine Listener/ListenerImpl, Demultiplexor/DemuxImpl

--- src/org/push/core/Listener.java
+++ src/org/push/core/Listener.java
@@ -1,10 +1,15 @@
 package org.push.core;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 import org.push.protocol.Protocol;
+import org.push.util.Debug;
 import org.push.util.Utils;
 
 /**
- * Facade of <code>ListenerImpl</code>
+ * This class provides a skeletal implementation for listening
+ * and accepting socket.
  * 
  * @author Lei Wang
  */
@@ -12,16 +17,23 @@ import org.push.util.Utils;
 public abstract class Listener {
 
 	protected ServerImpl serverImpl;
-	private ListenerImpl impl;
 
 	private int uPort;
 
 	protected ListenerOptions listenerOptions;
+	
+	// The server socket to listen to the port
+	private PushServerSocket serverSocket;
+
+	 // If listener is running.
+	private volatile boolean blnRunning;
+	
+	// Thread to accept the client socket
+	private Thread acceptorThread;
 
 	public Listener(ServerImpl serverImpl) {
 		Utils.nullArgCheck(serverImpl, "serverImpl");
 		this.serverImpl = serverImpl;
-		impl = new ListenerImpl(this);
 	    uPort = 0;
 	    listenerOptions = new ListenerOptions();
 	}
@@ -36,11 +48,108 @@ public abstract class Listener {
 	}
 
 	public boolean startListening() {
-		return impl.startListening();
+		// If already running
+		if (blnRunning) {
+			return true;
+		}
+
+		InetSocketAddress serverAddr = null;
+		String interfaceAddress = getOptions().getInterfaceAddress();
+		if(interfaceAddress != null) {
+			if (!"255.255.255.255".equals(interfaceAddress)) {
+				return false;
+			}
+
+			serverAddr = new InetSocketAddress(interfaceAddress, 
+					getListeningPort());
+	    } else {
+			serverAddr = new InetSocketAddress(getListeningPort());
+	    }
+
+	    blnRunning = doListening(serverAddr);
+	 
+	    return blnRunning;
 	}
 
 	public void stopListening() {
-		impl.stopListening();
+		blnRunning = false;
+
+	    // Close server socket
+		if (serverSocket != null) {
+			serverSocket.close();
+			serverSocket = null;
+		}
+		
+		// Wait the thread to terminate
+		if (acceptorThread != null) {
+			try {
+				acceptorThread.join();
+			} catch (InterruptedException e) {
+				//e.printStackTrace();
+			}
+
+			acceptorThread = null;
+		}
+	}
+	
+	private boolean doListening(InetSocketAddress serverAddr) {
+		boolean ret = false;
+		int socketBufferSize = getServerImpl().getServerOptions()
+					.getSocketBufferSize();
+		int socketType = getServerImpl().getServerOptions()
+					.getSocketType();
+		try {
+			// Create
+			serverSocket = SocketFactory.getDefault().createServerSocket(
+					socketType, socketBufferSize);
+			
+			// Bind
+			serverSocket.bind(serverAddr);
+
+			Debug.debug("Start to listen " + serverAddr.getHostName() + ":"
+					+ serverAddr.getPort());
+			
+			// Accept
+			doAccept();
+			
+			ret = true;
+		} catch (IOException e) {
+			e.printStackTrace();
+			if (serverSocket != null) {
+				serverSocket.close();
+				serverSocket = null;
+			}
+		}
+		
+		return ret;
+	}
+	
+	private void doAccept()
+	{
+		// Start a new thread
+		acceptorThread = new Thread(new Runnable()  {
+			public void run() {
+				while (blnRunning) {
+					try {
+						PushClientSocket clientSocket = serverSocket.accept();
+						
+						Debug.debug("New client from " + clientSocket.getIP());
+
+						// Start servicing the client connection
+						if (!handleAcceptedSocket(clientSocket)) {
+							clientSocket.close();
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+						return;
+					}
+				}
+			}
+			
+		});
+		
+		// Start the thread
+		acceptorThread.start();
 	}
 
 	public void setOptions(ListenerOptions listenerOptions) {
